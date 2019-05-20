880e1aefc204c77ce977e0f28837550e7693dd99
Seulgi Kim <7d4z7l@gmail.com>
2019-05-20 14:20:51 +0900

    [Mod] - modify name of Function from "map_0" to "map_block" map.c / interface.c / map.h : Modify name of Function from "map_0" to "map_block"
    map.h :
    delete unnecessary function - map_1(), map_2(), map_3()

--- map.h
+++ map.h
@@ -22,8 +22,8 @@ BOUNDARY boundary;
 int map_size;
 
 void map_boundary(int Map_Size);
-void map_0(int Map_Size);
-void map_1(void);void map_2(void);void map_3(void);void block_free(void);
+void map_block(int Map_Size);
+void block_free(void);
 BLOCK *block_create();
 
 #endif // MAP_H_INCLUDED
